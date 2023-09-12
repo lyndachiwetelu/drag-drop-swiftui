cdcf4b3a18c1d6cf73e49dd2f46fbfbae7167634
Lynda Chiwetelu <lyndachiwe@gmail.com>
2023-09-12 22:45:11 +0200

    Initial Commit

---
+++ Gestures/GesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GesturesApp.swift
+//  Gestures
+//
+//  Created by Lynda Chiwetelu on 12.09.23.
+//
+
+import SwiftUI
+
+@main
+struct GesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
